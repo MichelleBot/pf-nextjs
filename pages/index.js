177c9a72a83a0ae4c1e7f7fa9573ec5077aea879
Tsukari <146898194+MichelleBot@users.noreply.github.com>
2024-08-27 17:00:22 +0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+
+export default function Home() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>fahri | Spotify Music</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+        <link rel="stylesheet" href="https://nyimpen.vercel.app/component/spotfy.css" />
+      </Head>
+      <div className="container">
+        <header>
+          <img src="https://telegra.ph/file/66c5fd016d508e84175dd.jpg" alt="Profile Picture" className="profile-pic" />
+          <h1 className="nama">Pahrii</h1>
+          <p className="lokasi">
+            <i className="fas fa-globe-asia"></i> Jawa Tengah, Purwokerto
+          </p>
+          <p className="bio">”destroyer.”</p>
+        </header>
+
+        <section className="spotify-section">
+          <h2>The Playlist I Listen To</h2>
+          <div className="iframe-container">
+            {loading && (
+              <div className="loading">
+                <i className="fas fa-spinner"></i>
+              </div>
+            )}
+            <iframe
+              src="https://open.spotify.com/embed/playlist/5gYTcoy9MeWy1s3rTjoGFh"
+              frameBorder="0"
+              allowTransparency="true"
+              allow="encrypted-media"
+              style={{ display: loading ? 'none' : 'block' }}
+            ></iframe>
+          </div>
+        </section>
+
+        <div className="social-icons">
+          <a href="https://github.com/MichelleBot" target="_blank" className="social-link" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://wa.me/6289515999697?text=hi" target="_blank" className="social-link" rel="noopener noreferrer">
+            <i className="fab fa-whatsapp"></i>
+          </a>
+          <a href="https://instagram.com/" target="_blank" className="social-link" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://info-leoxr.vercel.app" target="_blank" className="social-link" rel="noopener noreferrer">
+            <i className="fas fa-arrow-up-right-from-square"></i>
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
